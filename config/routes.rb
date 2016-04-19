8a5248ed746f960aa706a1b99804c7e8b1600ce3
Iren Liber <irr.morgo@gmail.com>
2016-04-19 21:58:50 +0600

    removed junk files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :skip => 'registration'
-  resources :notes
+
 
   authenticated :user do
     root 'notes#index', as: "authenticated_root"
