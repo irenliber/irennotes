45b7ac49adb67d15ea8a6ffddcb6452cb2f32ed1
Iren Liber <irr.morgo@gmail.com>
2016-04-19 20:10:54 +0600

    deleted registration path

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
+  devise_for :users, :skip => 'registration'
   resources :notes
 
   authenticated :user do
     root 'notes#index', as: "authenticated_root"
   end
 
-  root 'welcome#index'
+  root 'notes#index'
 
 
 
